f0e565003b7525fc9477afef06416e167e22a5ff
Ilmee <ilmee.desilva@novigi.com.au>
2026-04-27 12:22:04 +0530

    fix: convert remaining double quotes to single quotes in HTML template
    - Fixed all double quotes in Swagger UI HTML template
    - Resolves remaining ESLint quote style violations

--- src/app.js
+++ src/app.js
@@ -114,12 +114,12 @@ app.get(["/api/docs", "/api/docs/"], (_req, res) => {
     <title>Tuk-Tuk Tracking API — Sri Lanka Police</title>
     <meta charset='utf-8'/>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+    <link rel='stylesheet' href='https://unpkg.com/swagger-ui-dist@5/swagger-ui.css'>
     <style>.swagger-ui .topbar { display: none }</style>
   </head>
   <body>
-    <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <div id='swagger-ui'></div>
+    <script src='https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js'></script>
     <script>
       window.onload = function() {
         const base = window.location.href.replace(/\\/api\\/docs\\/?.*$/, '/api/docs.json');
